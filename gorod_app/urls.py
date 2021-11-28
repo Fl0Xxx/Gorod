c983bdd457d213c8211a3546c195bb397a392586
Fl0Xxx <82512540+Fl0Xxx@users.noreply.github.com>
2021-11-29 01:22:02 +0200

    add registration, login, logout

--- gorod_app/urls.py
+++ gorod_app/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('test', views.test, name='test'),
+    path('account/create/', views.signUpView, name='signup'),
+    path('account/login/', views.login_view, name='login'),
+    path('account/logout/', views.logout_view, name='logout'),
 ]
+
